b1fc2cb949c30eb0a9f0749768dacd7479975b48
aasima007 <97961278+aasima007@users.noreply.github.com>
2022-01-21 22:07:08 +0530

    Update assigment 1.py

--- assigment 1.py
+++ assigment 1.py
@@ -5,9 +5,4 @@ pi=3.148
 a=pi*radius*radius
 
 print( "The area of circle with radius 1.1 is:" ,(a))
-
-2.file extension
-
-a=input("input the filename:")
-if".py" in a:
-    print("The extension of the file is:","'python'")
+ 
