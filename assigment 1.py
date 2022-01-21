92f621a4c43035b4d41b37b308674984ff865bce
aasima007 <97961278+aasima007@users.noreply.github.com>
2022-01-21 19:39:19 +0530

    Create assigment 1.py

---
+++ assigment 1.py
@@ -0,0 +1,13 @@
+1. area of circle
+
+radius=float(input("enter the radius of the circle:"))
+pi=3.148
+a=pi*radius*radius
+
+print( "The area of circle with radius 1.1 is:" ,(a))
+
+2.file extension
+
+a=input("input the filename:")
+if".py" in a:
+    print("The extension of the file is:","'python'")
